491c611b7813cdfbfd655c5be3525a7e9ce15830
George Townley <george@knowlesavenue.com>
2024-01-01 13:44:19 +0000

    initial commit

---
+++ snake-oil/src/components/styledbutton.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  background-color: blue;
+  color: white;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid blue;
+  border-radius: 3px;
+`;
+
+export default StyledButton;
